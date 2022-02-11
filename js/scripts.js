1ea89419d992cef79cbce968eb739ab0767020b3
kevintiamzon <98364071+kevintiamzon@users.noreply.github.com>
2022-02-11 09:14:56 -0800

    Created an index of Pokemon and added 3 Pokemon with name, height, and type values

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello World')
-
-let favoriteFood = 'Pasta';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+    {name: 'Charizard', height: 8, types:['fire', 'flying']},
+    {name: 'Jigglypuff', height: 1, types: ['normal', 'fairy']}
+];
