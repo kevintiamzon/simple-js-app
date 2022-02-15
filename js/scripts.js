6272ae0f9cd60d6a9904d7bad203e08075c50727
kevintiamzon <98364071+kevintiamzon@users.noreply.github.com>
2022-02-15 10:51:12 -0800

    Reformatted code and added a loop to display height of Pokemon and display message of largest Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
-    {name: 'Charizard', height: 8, types:['fire', 'flying']},
-    {name: 'Jigglypuff', height: 1, types: ['normal', 'fairy']}
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Charizard',
+        height: 8,
+        types:['fire', 'flying']
+    },
+    {
+        name: 'Jigglypuff',
+        height: 1,
+        types: ['normal', 'fairy']
+    }
 ];
+
+for (let i=0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height <= 7) {
+        document.write(pokemonList[i].name + '(height: ' + pokemonList[i].height + ')')
+    }
+    else if (pokemonList[i].height > 7) {
+        document.write(pokemonList[i].name + '(height: ' + pokemonList[i].height + ')' + "Wow, what a big Pokemon!")
+    }
+}
