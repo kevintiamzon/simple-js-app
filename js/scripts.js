77a169ee1e5c314d3caa7674262435a2283f2054
kevintiamzon <98364071+kevintiamzon@users.noreply.github.com>
2022-02-21 15:06:39 -0800

    Wrapped array in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,32 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 7,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Charizard',
-        height: 8,
-        types:['fire', 'flying']
-    },
-    {
-        name: 'Jigglypuff',
-        height: 1,
-        types: ['normal', 'fairy']
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 7,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Charizard',
+            height: 8,
+            types:['fire', 'flying']
+        },
+        {
+            name: 'Jigglypuff',
+            height: 1,
+            types: ['normal', 'fairy']
+        },
+    ]
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-];
+    function getAll() {
+        return pokemonList;
+    }
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
 
 function myLoopFunction(pokemonList) {
     if (pokemonList.height <= 7) {
@@ -24,4 +36,4 @@ function myLoopFunction(pokemonList) {
         document.write('<p>' + pokemonList.name + ' (height: ' + pokemonList.height + ') ' + "Wow, what a big Pokemon! " + '<p>');
     }
 }
-pokemonList.forEach(myLoopFunction);
+pokemonRepository.getAll().forEach(myLoopFunction);
