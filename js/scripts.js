320e30c51928218dcc7b8890c9252c22803c3ba2
kevintiamzon <98364071+kevintiamzon@users.noreply.github.com>
2022-02-22 11:53:07 -0800

    Created a button for each item in the array
    Added an event listener to run on a click to show more details about each pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -16,24 +16,43 @@ let pokemonRepository = (function () {
             types: ['normal', 'fairy']
         },
     ]
+
     function add(pokemon) {
         pokemonList.push(pokemon);
     }
+
     function getAll() {
         return pokemonList;
     }
+
+    function showDetails(pokemon){
+        console.log(pokemon);
+    }
+
+    function addListitem(pokemon){
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listpokemon = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('button-class');
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+        button.addEventListener('click', function(showDetails) {
+            console.log(pokemon);
+        })
+    }
+    
     return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListitem: addListitem
     };
 })();
 
-function myLoopFunction(pokemonList) {
-    if (pokemonList.height <= 7) {
-        document.write('<p>' + pokemonList.name + ' (height: ' + pokemonList.height + ') ' + '<p>');
-    }
-    else if (pokemonList.height > 7) {
-        document.write('<p>' + pokemonList.name + ' (height: ' + pokemonList.height + ') ' + "Wow, what a big Pokemon! " + '<p>');
-    }
-}
-pokemonRepository.getAll().forEach(myLoopFunction);
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name: 'Pikachu', height: 0.3, types: ['electric'] });
+
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListitem(pokemon);
+});
